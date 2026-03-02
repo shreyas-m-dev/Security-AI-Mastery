aff5d7b9b69544868bca0ffce69be44749c01411
shreyas-m-dev <shreyasgowda3016@gmail.com>
2026-03-02 12:34:35 +0000

    Day 1: Final working age calculator with typecasting

---
+++ Day-1/age_calc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+name = input('Enter your Name:')
+age = int(input('Enter your Age:'))
+curr_year = 2026
+birth_year = curr_year - age
+print('You were born in the year:',birth_year)
+ 
+
