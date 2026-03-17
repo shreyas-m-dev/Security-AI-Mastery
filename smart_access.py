c14c907ef5d3f879ec9f56351a63455b0c573cd1
shreyas-m-dev <shreyasgowda3016@gmail.com>
2026-03-17 14:36:02 +0000

    Day 3: Automated Continuous Access Monitot using Sentinel Loops

--- smart_access.py
+++ smart_access.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-name = input('Enter your name:')
-age = int(input('Enter your age:'))
-if age<0 or age>120:
-	print('ALERT: logical impossibility detected. Terminating.')
-elif age<18:
-	print(f'Access restricted for {name}. Status:Minor')
-else:
-	print(f'Access granted for {name}. Status:Authorized')
+while True:
+	print('\n---- New Access Request ----')
+	name = input("Enter name (or type 'exit' to stop):")
+	if name.lower()=='exit':
+		print('Shutting down monitor. Goodbye!')
+		break
+	try:
+		age = int(input('Enter age:'))
+		if age<0 or age>120:
+			print('Alert! logical impossibility detected.')
+		elif age<18:
+			print(f'Access Restricted for {name}. Status: Minor')
+		else:
+			print(f'Access Granted for {name}. Status: Authorized')
+	except ValueError:
+		print('Error: Please enter a valid number of age.')
 
